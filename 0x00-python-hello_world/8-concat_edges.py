3a6bb085b75a2c7ebb1332a9a002f7d4d3e9c972
Ahmed Mansour <ahmedkamel120012@gmail.com>
2024-02-03 21:15:49 +0200

    U tasks

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[str.find("object"):str.find("language")], end="")
-print(f"{str[str.find('with'):str.find('very')]}{str[:6]}", end="\n")
+str = str[39:67] + str[-22:-17] + str[:6]
+print(str)
