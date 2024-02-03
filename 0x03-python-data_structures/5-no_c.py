209c385d42d6c2d1c5fc74de61e5ee3d8e05f368
Ahmed Mansour <ahmedkamel120012@gmail.com>
2024-02-03 19:35:20 +0200

    Tasks

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    result_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            result_string += char
+
+    return result_string     
